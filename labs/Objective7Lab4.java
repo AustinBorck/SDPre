5be3d4457017439ec8fd1609cf9f9bb58763ece1
Austin Borck <austinborck98@gmail.com>
2022-05-24 17:15:25 -0600

    Objective7Lab4

---
+++ labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+int count = 1;
+int sum = 1;
+
+while(count <= 20) {
+  count = count + 1;
+  sum = sum + count;
+  //count = count + sum;
+  //System.out.println(sum);
+}
+System.out.println(sum);
+    //end
+  }
+}
