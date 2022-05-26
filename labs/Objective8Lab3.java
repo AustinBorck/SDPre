050c07325fd73fcc3811ad579cdf77cb956a209f
Austin Borck <austinborck98@gmail.com>
2022-05-26 17:22:55 -0600

    Objective8Lab3

---
+++ labs/Objective8Lab3.java
@@ -0,0 +1,16 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+
+for (int x=1; x<=20; x++){
+  if(x % 2 == 0){
+    System.out.println(x + " is even.");
+  }
+  if(x % 2 != 0){
+    System.out.println(x + " is odd.");
+  }
+}
+
+  //end
+  }
+}
