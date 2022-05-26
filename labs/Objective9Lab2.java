ba4ba24225865950a4d0b32b2b12c1c4bc58bbe0
Austin Borck <austinborck98@gmail.com>
2022-05-26 17:45:25 -0600

    Objective9Lab2

---
+++ labs/Objective9Lab2.java
@@ -0,0 +1,19 @@
+public class Objective9Lab2 {
+  public static void main(String[] args){
+
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+
+    //end main
+  }
+public static void printMessage(){
+System.out.println("In method");
+
+
+  //end message
+}
+
+
+  //end class
+}
