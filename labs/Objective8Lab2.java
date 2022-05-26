7d4380f2c87a09b5f6a7746c60408b0b4d4aae97
Austin Borck <austinborck98@gmail.com>
2022-05-26 17:12:01 -0600

    Objective8Lab2

---
+++ labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+int total=0;
+for (int x=1; x<=20; x++){
+  total += x;
+}
+System.out.println(total);
+
+    //end
+  }
+}
